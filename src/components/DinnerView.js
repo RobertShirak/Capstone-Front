85d822fe95ac87b3c732bb170876aa63c97736ef
Robert Shirak <bcshirak1@gmail.com>
2020-12-18 09:25:31 -0500

    freezed

--- src/components/DinnerView.js
+++ src/components/DinnerView.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { BrowserRouter as Router} from 'react-router-dom'
 import { Container } from '@material-ui/core';
 import { Button } from '@material-ui/core';
-import {  Box } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
